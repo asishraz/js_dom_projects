d5b690b121dc2aa244f540c1e45e2548eb45d914
asishraz <iamasish3001@gmail.com>
2021-07-26 21:08:44 +0530

    question asked

--- color_picker/main.js
+++ color_picker/main.js
@@ -1,11 +1,35 @@
-//compare two arrays
-function compareArrays(array1, array2) {
-  var a = array1.sort();
-  var b = array2.sort();
-  for (var i = 0; i < a.length; i++) {
-    if (a[i] != b[i]) {
-      return false;
-    }
-  }
-  return true;
+console.log(a);
+console.log(b);
+console.log(c);
+var a = 10;
+let b = 20;
+const c = 30;
+
+
+
+const arr = [1,2,3];
+
+const arr2 = [...arr,4,5,6];
+
+let obj1 = {
+  firstname: 'asish',
 }
+
+let obj2 = obj1;
+
+obj2.firstname = 'rahul';
+console.log(obj1);
+console.log(obj2);
+
+const sum = (a,b,c) => {
+};
+
+
+const arr = [10,20,30,150,15];
+
+function sortedArr(arr) {
+  
+
+}
+
+
