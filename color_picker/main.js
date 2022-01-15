2ebe5736455c34048c6cafd68145af0e3572a3e8
asishraz <iamasish3001@gmail.com>
2022-01-15 11:31:14 +0530

    random quotes work start

--- color_picker/main.js
+++ color_picker/main.js
@@ -5,31 +5,22 @@ var a = 10;
 let b = 20;
 const c = 30;
 
+const arr = [1, 2, 3];
 
-
-const arr = [1,2,3];
-
-const arr2 = [...arr,4,5,6];
+const arr2 = [...arr, 4, 5, 6];
 
 let obj1 = {
-  firstname: 'asish',
-}
+  firstname: "asish",
+};
 
 let obj2 = obj1;
 
-obj2.firstname = 'rahul';
+obj2.firstname = "rahul";
 console.log(obj1);
 console.log(obj2);
 
-const sum = (a,b,c) => {
-};
+const sum = (a, b, c) => {};
 
+const arr = [10, 20, 30, 150, 15];
 
-const arr = [10,20,30,150,15];
-
-function sortedArr(arr) {
-  
-
-}
-
-
+function sortedArr(arr) {}
